0f88249d1303e21866890cd236ed281a78e5523f
Saverio Trioni <saverio.trioni@gmail.com>
2021-10-29 20:36:10 +0200

    Some content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
 
+import Link from 'next/link'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header>
+        <p>
+          I’m Sarah Bonavia.
+          I’m visual and multifunctional.
+        </p>
+        <nav>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/work">Work</Link></li>
+          <li><Link href="/cv">CV</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </nav>
+      </header>
+      <Component {...pageProps} />
+      <footer>
+        © 2021 Sarah Bonavia
+      </footer>
+    </>
+  )
 }
 
 export default MyApp
